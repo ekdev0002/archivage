1c35372a5ccef1363d41319778d3de7032a52e93
yabyoure <yabyourekabore@gmail.com>
2020-01-15 00:34:35 +0000

    first commit secure only by session

--- vues/filtre_securite.php
+++ vues/filtre_securite.php
@@ -1,43 +1,25 @@
 <?php
 session_start();
  
-if (isset($_SESSION['profil'])) {
+if (isset($_SESSION['identifiant'])) {
 	$url=$_SERVER["REQUEST_URI"];
 
 	$path=str_replace("/archivage/vues/","",$url);
 	
-	$pos=strpos($path,'?',0);
-	$newpath=substr($path,0,$pos);	
-	
-	$file=$newpath;	
+	$file=$path;
 	
 	if (file_exists($file)) {
 		
-		$dir="temp/";
-		if(is_dir($dir)===false){
-            mkdir($dir);
-			$ext = pathinfo($file, PATHINFO_EXTENSION);
-			$filetemp=$dir."fichier_a_telecharger.".$ext;
-			copy($file, $filetemp);
-        }
-		else{
-			$ext = pathinfo($file, PATHINFO_EXTENSION);
-			$filetemp=$dir."fichier_a_telecharger.".$ext;
-			copy($file, $filetemp);			
-		}
 		
 		header('Content-Description: File Transfer');
 		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename="'.basename($filetemp).'"');
+		header('Content-Disposition: attachment; filename="'.basename($file).'"');
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate');
 		header('Pragma: public');
-		header('Content-Length: ' . filesize($filetemp));
-		readfile($filetemp);
+		header('Content-Length: ' . filesize($file));
+		readfile($file);
 		
-		//suppression du repertoire temporaire 
-		unlink($filetemp);
-		rmdir($dir);
 		header("Location: http://127.0.0.1/archivage/"); 
 		exit;
 	}
