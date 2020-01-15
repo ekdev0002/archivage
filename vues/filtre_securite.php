d470789539b899ca91c9e72bc3e42163f89294c5
yabyoure <yabyourekabore@gmail.com>
2020-01-15 00:48:20 +0000

    first commit secure session temp folder

--- vues/filtre_securite.php
+++ vues/filtre_securite.php
@@ -9,17 +9,32 @@ if (isset($_SESSION['identifiant'])) {
 	$file=$path;
 	
 	if (file_exists($file)) {
+		$dir="temp/";
+		if(is_dir($dir)===false){
+            mkdir($dir);
+			$ext = pathinfo($file, PATHINFO_EXTENSION);
+			$filetemp=$dir."fichier_a_telecharger.".$ext;
+			copy($file, $filetemp);
+        }
+		else{
+			$ext = pathinfo($file, PATHINFO_EXTENSION);
+			$filetemp=$dir."fichier_a_telecharger.".$ext;
+			copy($file, $filetemp);			
+		}
 		
 		
 		header('Content-Description: File Transfer');
 		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename="'.basename($file).'"');
+		header('Content-Disposition: attachment; filename="'.basename($filetemp).'"');
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate');
 		header('Pragma: public');
-		header('Content-Length: ' . filesize($file));
-		readfile($file);
+		header('Content-Length: ' . filesize($filetemp));
+		readfile($filetemp);
 		
+		//suppression du repertoire temporaire 
+		unlink($filetemp);
+		rmdir($dir);
 		header("Location: http://127.0.0.1/archivage/"); 
 		exit;
 	}
